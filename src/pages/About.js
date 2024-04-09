2bccb05b7305874f4961fcdf04b074906052cfd5
leonliu1008 <leonliu1008@gmail.com>
2024-04-09 13:40:20 +0800

    add about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h3>關於網站</h3>
+      <p>歡迎來到我的網站！這是一個使用搜尋照片 API 所開發的照片搜尋網站。</p>
+
+      <h3>我的宗旨</h3>
+      <p>
+        讓人們更方便地搜尋照片。無論是尋找特定主題的照片，還是瀏覽各種風格的圖像，讓用戶可以輕鬆地找到所需的照片。
+      </p>
+
+      <h3>關於我</h3>
+      <p>
+        我是一個熱愛寫程式的工程師，正在朝著全端工程師的目標發展。致力於提供最佳的網站體驗。
+      </p>
+    </div>
+  );
 };
 
 export default About;
